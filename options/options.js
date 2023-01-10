a6c43ee73daf094d30459101868071cfb0095195
Crowdbotics <team@crowdbotics.com>
2023-01-10 15:30:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lockedin-38243",
+  url: "https://lockedin-38243.botics.co",
+  api: "https://lockedin-38243.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
